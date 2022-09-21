a5668a4028211add9cf62a6784a083fb261743e6
niloy143 <niloymahmudapu@gmail.com>
2022-09-22 00:50:51 +0600

    count

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
@@ -14,21 +14,23 @@ function Users() {
   const [users, setUsers] = useState([]);
   useEffect(() => {
     fetch('https://randomuser.me/api/?results=100')
-    .then(res => res.json())
-    .then(data => setUsers(data.results))
+      .then(res => res.json())
+      .then(data => setUsers(data.results))
   }, []);
+  let count = 1;
   return (
     <div className='grid'>
-      {users.map(user => <User user={user}></User>)}
+      {users.map(user => <User user={user} count={count++}></User>)}
     </div>
   )
 }
 
 function User(props) {
-  const {picture, name, email} = props.user;
+  const { picture, name, email } = props.user;
   return (
     <div>
       <img src={picture.large} alt="" />
+      <h2>{props.count}</h2>
       <h3>{name.title} {name.first} {name.last}</h3>
       <p>{email}</p>
     </div>
