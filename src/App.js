3ca8f90355472ac9c8429f75c3e3409a2b916b95
niloy143 <niloymahmudapu@gmail.com>
2022-09-21 22:14:05 +0600

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Users></Users>
     </div>
   );
+}
+
+function Users() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch('https://randomuser.me/api/?results=100')
+    .then(res => res.json())
+    .then(data => setUsers(data.results))
+  }, []);
+  return (
+    <div className='grid'>
+      {users.map(user => <User user={user}></User>)}
+    </div>
+  )
+}
+
+function User(props) {
+  const {picture, name, email} = props.user;
+  return (
+    <div>
+      <img src={picture.large} alt="" />
+      <h3>{name.title} {name.first} {name.last}</h3>
+      <p>{email}</p>
+    </div>
+  )
 }
 
 export default App;
